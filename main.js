35d0059059c9b96b48ae41431ef8131d597d6c3c
AmarKolipe <87370429+AmarKolipe@users.noreply.github.com>
2022-05-12 19:08:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+Webcam.set({
+
+    width:350,
+
+    height:300,
+
+    image_format:'png',
+
+    png_quality:90
+
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+
+    Webcam.snap(function(data_uri){
+
+        document.getElementById("result").innerHTML = '<img id="captured_face" src="'+data_uri+'"/>';
+
+    });
+
+}
+
+console.log('ml5 Version: ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EmWZaH8up/model.json', modelLoaded);
+
+function modelLoaded()
+{
+
+    console.log("Model Loaded");
+
+}
+
+function identify()
+{
+
+    img = document.getElementById("captured_image");
+
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result)
+{
+
+    if(error)
+    {
+
+        console.log(error);
+
+
+    }
+
+    else
+    {
+
+        console.log(result);
+
+        document.getElementById("result_face_name").innerHTML = results[0].label;
+    
+        document.getElementById("result_face_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
